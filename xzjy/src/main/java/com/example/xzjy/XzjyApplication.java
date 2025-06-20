c5168587d808a62d7180af57a2479736e64e4810
859025462 <121717136+859025462@users.noreply.github.com>
2025-06-20 17:50:20 +0800

    创建后端框架

---
+++ xzjy/src/main/java/com/example/xzjy/XzjyApplication.java
@@ -0,0 +1,13 @@
+package com.example.xzjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XzjyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XzjyApplication.class, args);
+	}
+
+}
